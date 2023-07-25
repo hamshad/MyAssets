cbddc8ed77621c671598f4314df5e0cfad4817f5
hamshad <hiromega18@gmail.com>
2023-07-25 18:31:37 +0530

    Initial Commit

--- app/src/main/java/com/example/myassets/TextError/TextErrorActivity.java
+++ app/src/main/java/com/example/myassets/TextError/TextErrorActivity.java
@@ -10,6 +10,9 @@ import com.example.myassets.R;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TextErrorActivity extends AppCompatActivity {
 
     TextInputLayout loginUsernameTextLayout;
@@ -35,9 +38,30 @@ public class TextErrorActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.length() > 8) {
-                    inputLayout.setError("Less than 8 Character");
+            public void onTextChanged(CharSequence password, int i, int i1, int i2) {
+
+                //For strong password validation:
+
+//                ^ represents starting character of the string.
+//                (?=.*[0-9]) represents a digit must occur at least once.
+//                (?=.*[a-z]) represents a lower case alphabet must occur at least once.
+//                (?=.*[A-Z]) represents an upper case alphabet that must occur at least once.
+//                (?=.*[@#$%^&-+=()] represents a special character that must occur at least once.
+//                (?=\\S+$) white spaces don’t allowed in the entire string.
+//                .{8, 20} represents at least 8 characters and at most 20 characters.
+//                 $ represents the end of the string.
+                if (password.length() < 6 || password.length() > 20) {
+                    inputLayout.setError("Password should be between 6 to 20 characters");
+                } else if (regexSearch(password ,"[A-Z]")) {
+                    inputLayout.setError("At least one Uppercase letter is needed");
+                } else if (regexSearch(password, "[0-9]")) {
+                    inputLayout.setError("At least one Number is needed");
+                } else if (regexSearch(password, "\\S+$")) {
+                    inputLayout.setError("Remove empty space");
+                } else if (regexSearch(password, "[a-z]")) {
+                    inputLayout.setError("At least one Lowercase letter is needed");
+                } else if (regexSearch(password, "[@#$%^&-_]")) {
+                    inputLayout.setError("At least one Special Character is needed (eg.@#$%^&-_)");
                 } else {
                     inputLayout.setError(null);
                 }
@@ -48,5 +72,14 @@ public class TextErrorActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean regexSearch(CharSequence password ,String regex) {
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        return !matcher.find();
+
     }
 }
